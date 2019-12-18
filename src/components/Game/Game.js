b9f406078aa2696234fa4234f662d322dd127d1c
Christopher Gottshalk Jr <christophergottshalkjr@gmail.com>
2019-12-17 23:02:32 -0500

    working on game logic in game.js, adding navbar and click functionality.

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -5,6 +5,11 @@ import Wrapper from "../Wrapper";
 import characters from "../../characters.json";
 import "./game.css";
 
+let correctClicks = 0;
+let score = 0;
+let topScore = 0;
+let message;
+
 
 
 
@@ -12,7 +17,72 @@ import "./game.css";
 class Game extends Component {
 
     state = {
-        characters
+        characters,
+        correctClicks,
+        score,
+        topScore,
+        message
+    };
+
+    whenClicked = char => {
+
+        const characters = this.state.characters;
+
+        const correctClick = characters.filter(click => click.id === char);
+
+        if (correctClick[0].clicked) {
+
+            score = 0;
+            message = "WRONG CHOICE!!!!"
+
+            for (let i = 0; i < characters.length; i++) {
+                characters[i].clicked = false;
+            }
+
+            this.setState({ message });
+            this.setState({ correctClicks });
+            this.setState({ characters });
+
+        } else if (correctClicks < 11) {
+
+
+            correctClick[0].clicked = true;
+
+            correctClicks++;
+
+            message = "Hooray!!!!! Keep it Goin' Bucko!!!";
+
+            if (correctClicks > topScore) {
+                topScore = correctClicks;
+                this.setState({ topScore });
+            }
+
+            characters.sort(function (a, b) { return 0.5 - Math.random() });
+
+            this.setState({ characters });
+            this.setState({ correctClicks });
+            this.setState({ message });
+        } else {
+
+            correctClick[0].clicked = true;
+
+            correctClicks = 0;
+
+            message = "TOP FORM OLE SPORT! PLAY AGAIN!!!!"
+            topScore = 12;
+            this.setState({ topScore });
+
+            for (let i = 0; i < characters.length; i++) {
+                characters[i].clicked = false;
+            }
+
+            characters.sort(function (a, b) { return 0.5 - Math.random() });
+
+            this.setState({ characters });
+            this.setState({ correctClicks });
+            this.setState({ message });
+
+        }
     };
 
     render() {
@@ -31,6 +101,7 @@ class Game extends Component {
                                             key={char.id}
                                             name={char.name}
                                             image={char.image}
+                                            whenClicked={this.whenClicked}
                                         />
                                     </div>
                                 ))
--- src/components/SagaChar/SagaChar.js
+++ src/components/SagaChar/SagaChar.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./saga.css";
 
 const SagaChar = props => (
-    <div className="card box-shadow--16dp m-3" id={props.id} onClick={props.handleClick}>
+    <div className="card box-shadow--16dp m-3" id={props.id} onClick={props.whenClicked}>
        <img className="card-img-top img-fluid" src={props.image} alt={props.name} />
     </div>
 );
