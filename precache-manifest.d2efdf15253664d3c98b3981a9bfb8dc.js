a179860e8edca2a0a3aefa90965be7bd8a9a16a0
Christopher Gottshalk Jr <christophergottshalkjr@gmail.com>
2019-12-19 21:50:25 -0500

    Updates

---
+++ precache-manifest.d2efdf15253664d3c98b3981a9bfb8dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c314e7a40ad0a4fc3b11b64210e1fbb",
+    "url": "/sagaclickygame/index.html"
+  },
+  {
+    "revision": "f18290432e502289aac1",
+    "url": "/sagaclickygame/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "01620bd7b6d05d319fdb",
+    "url": "/sagaclickygame/static/css/main.95323fc5.chunk.css"
+  },
+  {
+    "revision": "f18290432e502289aac1",
+    "url": "/sagaclickygame/static/js/2.8ef5f046.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/sagaclickygame/static/js/2.8ef5f046.chunk.js.LICENSE"
+  },
+  {
+    "revision": "01620bd7b6d05d319fdb",
+    "url": "/sagaclickygame/static/js/main.66cf5f83.chunk.js"
+  },
+  {
+    "revision": "5dcde11babd0693ccf47",
+    "url": "/sagaclickygame/static/js/runtime-main.245bc193.js"
+  }
+]);
